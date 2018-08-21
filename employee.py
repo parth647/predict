7d55aef7bd672b7b303b17d6ef5e8244d1a92489
Parth <parth647@gmail.com>
2018-08-22 00:48:20 +0530

    initiated

---
+++ employee.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 22 00:08:01 2018
+
+@author: parth
+"""
+
+#import modules
+import pandas  # for dataframes
+import matplotlib.pyplot as plt # for plotting graphs
+import seaborn as sns # for plotting graphs
