41c540ad73bf0398561dd79c633890ed1da22b3f
Parth <parth647@gmail.com>
2018-08-22 17:57:04 +0530

    Data analysis completed

--- employee.py
+++ employee.py
@@ -9,3 +9,21 @@ Created on Wed Aug 22 00:08:01 2018
 import pandas  # for dataframes
 import matplotlib.pyplot as plt # for plotting graphs
 import seaborn as sns # for plotting graphs
+data=pandas.read_csv('HR_comma_sep.csv')
+left_count=data.groupby('left').count()
+features=['number_project','time_spend_company','Work_accident','left', 'promotion_last_5years','Departments ','salary']
+fig=plt.subplots(figsize=(10,15))
+for i, j in enumerate(features):
+    plt.subplot(4, 2, i+1)
+    plt.subplots_adjust(hspace = 1.0)
+    sns.countplot(x=j,data = data)
+    plt.xticks(rotation=90)
+    plt.title("No. of employee")
+    
+fig=plt.subplots(figsize=(10,15))
+for i, j in enumerate(features):
+    plt.subplot(4, 2, i+1)
+    plt.subplots_adjust(hspace = 1.0)
+    sns.countplot(x=j,data = data, hue='left')
+    plt.xticks(rotation=90)
+    plt.title("No. of employee")
