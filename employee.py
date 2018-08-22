29dce4e6d82a099dac1e37c83325a404b97b8be0
Parth <parth647@gmail.com>
2018-08-22 23:58:59 +0530

    cluster

--- employee.py
+++ employee.py
@@ -27,3 +27,19 @@ for i, j in enumerate(features):
     sns.countplot(x=j,data = data, hue='left')
     plt.xticks(rotation=90)
     plt.title("No. of employee")
+
+#import module
+from sklearn.cluster import KMeans
+# Filter data
+left_emp =  data[['satisfaction_level', 'last_evaluation']][data.left == 1]
+# Create groups using K-means clustering.
+kmeans = KMeans(n_clusters = 3, random_state = 0).fit(left_emp)
+
+# Add new column "label" annd assign cluster labels.
+left_emp['label'] = kmeans.labels_
+# Draw scatter plot
+plt.scatter(left_emp['satisfaction_level'], left_emp['last_evaluation'], c=left_emp['label'],cmap='Accent')
+plt.xlabel('Satisfaction Level')
+plt.ylabel('Last Evaluation')
+plt.title('3 Clusters of employees who left')
+plt.show()
